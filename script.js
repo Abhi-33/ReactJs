55ec26c5f5b89da3fce972015e6f3ba374649a0b
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-03-26 16:09:47 +0530

    Initial commits

---
+++ script.js
@@ -0,0 +1,22 @@
+// 'use strict';
+
+// let a = 34;
+// a = 'This is me';
+// console.log(a);
+
+// function check(){
+//     // confirm('are you sure ?');
+//     console.log(confirm);
+// }
+// check();
+
+// setTimeout(() => {
+//     console.log('yayy tu sikh gaya');
+// }, 2000);
+
+// function Car(props){
+//    return <h1>I am a {props.brand} Car</h1>
+// }
+
+// const myElement = <car brand='Mercedes'/>
+// document.write(myElement);
