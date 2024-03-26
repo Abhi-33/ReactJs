d783bfc49ca37a2de92470afa788a74ad5f9cfd1
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-03-26 16:05:22 +0530

    Initial commits

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar.js';
+
+function App() {
+  return (
+    <>
+  <Navbar title={3} aboutText='About Text'/>
+  </>
+    );
+}
+
+export default App;
